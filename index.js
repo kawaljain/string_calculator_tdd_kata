9b788804c9d5d4a38f8c7abe10e9b399b3698b99
Kawal Jain <kawaljain03@gmail.com>
2024-12-12 07:02:23 +0530

    [][code refactor for new line test case]

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
 class StringCalculator {
+  getSplitNumber = (numbers = "") => {
+    let numberList = [];
+    let regexMatch = /,|\n/;
+    for (let i = 0; i < numbers.length; i++) {
+      if (!numbers[i].match(regexMatch)) {
+        numberList.push(parseInt(numbers[i]));
+      }
+    }
+    return numberList;
+  };
+
   add(numbers) {
     if (!numbers) {
       return 0;
     }
 
-    const getSplitNumber = (numbers, splitBy = ",", hasNew = false) => {
-      let list = numbers.split(splitBy);
-      if (splitBy === ",") {
-        return list;
-      } else {
-        for (let i = 0; i < list.length; i++) {
-          if (list[i].match(/,/g)) {
-            let temp = list[i];
-            list.splice(i, 1);
-            list = [...list, ...getSplitNumber(temp)];
-          }
-        }
-        return list;
-      }
-    };
-
-    let splitBy = numbers.match(/\n/g) ? "\n" : ",";
-    const numArray = getSplitNumber(numbers, splitBy);
-
+    const numArray = this.getSplitNumber(numbers);
     if (numArray.length <= 1) {
       return parseInt(numbers);
     }
+
     let sum = 0;
     sum = numArray.reduce((x, y) => parseInt(x) + parseInt(y), sum);
     return sum;
   }
 }
-// let calculator = new StringCalculator();
-// calculator.add("1\n2,3");
+let calculator = new StringCalculator();
+calculator.getSplitNumber("1\n2,3");
 export default StringCalculator;
