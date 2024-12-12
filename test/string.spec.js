4aef013857271dcaeac049d92d681aaafdff348c
Kawal Jain <kawaljain03@gmail.com>
2024-12-12 05:35:36 +0530

    [][minor fix- importing calculator]

--- test/string.spec.js
+++ test/string.spec.js
@@ -1,5 +1,6 @@
 import { describe } from "mocha";
 import { expect } from "chai";
+import StringCalculator from "./../index.js";
 
 describe("String Calculator", () => {
   it("should 0 when given an empty string", () => {
