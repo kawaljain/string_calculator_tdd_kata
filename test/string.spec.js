a9c459b53ea1a39fa35f785a82fd3d95dd7e53b2
Kawal Jain <kawaljain03@gmail.com>
2024-12-12 05:42:50 +0530

    [][refactor spec file ]

--- test/string.spec.js
+++ test/string.spec.js
@@ -3,14 +3,16 @@ import { expect } from "chai";
 import StringCalculator from "./../index.js";
 
 describe("String Calculator", () => {
+  let calculator;
+  beforeEach(() => {
+    calculator = new StringCalculator();
+  });
   it("should 0 when given an empty string", () => {
-    let calculator = new StringCalculator();
     expect(calculator.add("")).to.equal(0);
   });
 
   describe("Returns the number itself if only one number is provided", () => {
     it("return 1, when given number is 1", () => {
-      let calculator = new StringCalculator();
       expect(calculator.add("1")).to.equal(1);
     });
   });
