ef914bd667a6fb5b979988359d0755928ba4d863
Kawal Jain <kawaljain03@gmail.com>
2024-12-12 05:38:46 +0530

    [][fixing 2 test cases number itself if only one number is provided]

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ class StringCalculator {
     if (!numbers) {
       return 0;
     }
+    const numArray = numbers.split(",");
+    if (numArray.length <= 1) {
+      return parseInt(numbers);
+    }
   }
 }
 
