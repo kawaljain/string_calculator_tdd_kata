33c156d0c8754e83f695e2b68e17f66347b3207f
Kawal Jain <kawaljain03@gmail.com>
2024-12-12 06:46:20 +0530

    [][test case fix for new line between number]

--- index.js
+++ index.js
@@ -3,7 +3,26 @@ class StringCalculator {
     if (!numbers) {
       return 0;
     }
-    const numArray = numbers.split(",");
+
+    const getSplitNumber = (numbers, splitBy = ",", hasNew = false) => {
+      let list = numbers.split(splitBy);
+      if (splitBy === ",") {
+        return list;
+      } else {
+        for (let i = 0; i < list.length; i++) {
+          if (list[i].match(/,/g)) {
+            let temp = list[i];
+            list.splice(i, 1);
+            list = [...list, ...getSplitNumber(temp)];
+          }
+        }
+        return list;
+      }
+    };
+
+    let splitBy = numbers.match(/\n/g) ? "\n" : ",";
+    const numArray = getSplitNumber(numbers, splitBy);
+
     if (numArray.length <= 1) {
       return parseInt(numbers);
     }
@@ -12,5 +31,6 @@ class StringCalculator {
     return sum;
   }
 }
-
+// let calculator = new StringCalculator();
+// calculator.add("1\n2,3");
 export default StringCalculator;
